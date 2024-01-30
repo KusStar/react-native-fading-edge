2ed414a0f5f885db821778aa799f056bbe769bdf
kusstar <kussssss@outlook.com>
2024-01-30 22:28:40 +0800

    fix: typings

--- index.d.ts
+++ index.d.ts
@@ -4,17 +4,18 @@ import * as React from 'react';
 import * as ReactNative from 'react-native';
 
 interface FadingEdgeProps extends ReactNative.ViewProps {
-  top: number;
-  left: number;
-  bottom: number;
-  right: number;
+  // defaults to 0
+  top?: number;
+  // defaults to 0
+  left?: number;
+  // defaults to 0
+  bottom?: number;
+  // defaults to 0
+  right?: number;
 }
 /**
  * @see https://github.com/KusStar/react-native-fading-edge
  */
 declare class FadingEdgeComponent extends React.Component<FadingEdgeProps> {}
-declare const FadingEdgeBase: ReactNative.Constructor<
-  ReactNative.NativeMethodsMixin
-> &
-  typeof FadingEdgeComponent;
+declare const FadingEdgeBase: typeof FadingEdgeComponent;
 export default class FadingEdge extends FadingEdgeBase {}
